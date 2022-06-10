9d368a94038b79a931b654430d2d1a20c0c767b8
ali navidi <ali_navidi24@yahoo.com>
2022-06-10 22:00:11 +0430

    followers added!

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true, minlength: 10 },
-    creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
-    image: { type: String, required: true }
-   
-})
+  title: { type: String, required: true },
+  description: { type: String, required: true, minlength: 10 },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  image: { type: String, required: true },
+  date: { type: String, required: true },
+});
 
-module.exports = mongoose.model('Post', postSchema)
+module.exports = mongoose.model("Post", postSchema);
--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true, minlength: true },
   image: { type: String, required: true },
-  posts: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Post' }],
+  followings: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }],
+  followers: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }],
+  posts: [{ type: mongoose.Types.ObjectId, required: true, ref: "Post" }],
 });
 
 module.exports = mongoose.model("User", userSchema);
