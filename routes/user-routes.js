0fd6a8303f90cd976acb559510b2d6c1c88529d4
ali navidi <ali_navidi24@yahoo.com>
2022-07-03 00:15:04 +0430

    timeline posts added!

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/user-controllers");
 
@@ -9,15 +8,7 @@ router.get("/", usersControllers.getUsers);
 
 router.get("/:uid", usersControllers.getUserById);
 
-router.post(
-  "/signup",
-  [
-    check("username").not().isEmpty(),
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
-  usersControllers.signup
-);
+router.post("/signup", usersControllers.signup);
 
 router.post("/login", usersControllers.login);
 
