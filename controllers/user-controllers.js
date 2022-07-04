75aa828de461982f1939135c700d2819dec2f1e9
ali navidi <ali_navidi24@yahoo.com>
2022-07-04 14:55:29 +0430

    test

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,7 +1,6 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
