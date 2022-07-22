725f67b8152e1e6c46e2147416e0e137b31e67c1
ali navidi <ali_navidi24@yahoo.com>
2022-07-23 00:24:18 +0430

    Post schema changed!

--- models/posts.js
+++ models/posts.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String },
+  description: { type: String },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   song: { type: String, required: true },
   date: { type: String, required: true },
