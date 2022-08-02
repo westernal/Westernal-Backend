1c6af1efa1325bf109b5aee419ebf4744d256b41
ali navidi <ali_navidi24@yahoo.com>
2022-08-03 00:03:36 +0430

    some minor changes

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true, minlength: true },
-  image: { type: String, default: "/uploads/userIcon.png" },
+  image: { type: String, default: "uploads/userIcon.png" },
   bio: { type: String },
   followings: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }],
   followers: [{ type: mongoose.Types.ObjectId, required: true, ref: "User" }],
