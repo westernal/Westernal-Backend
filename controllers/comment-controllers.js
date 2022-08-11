32d4e1ba7bac93323b9ff34dc4695a184f1fae8c
ali navidi <ali_navidi24@yahoo.com>
2022-08-12 04:07:08 +0430

    replies added to the comments

--- controllers/comment-controllers.js
+++ controllers/comment-controllers.js
@@ -62,6 +62,78 @@ const postComment = async (req, res, next) => {
   res.status(201).json({ message: "Comment posted!" });
 };
 
+const postReply = async (req, res, next) => {
+  const { writerId, postId, message, commentId } = req.body;
+
+  const commentDate = new Date();
+  let post;
+  let user;
+  let comment;
+
+  try {
+    user = await User.findById(writerId);
+  } catch (error) {
+    next(error);
+  }
+
+  if (!user) {
+    const error = new HttpError("User doesn't exist.", 500);
+    return next(error);
+  }
+
+  const postedComment = new Comment({
+    writer: { id: writerId, username: user.username, avatar: user.image },
+    postId: postId,
+    message: message,
+    date: commentDate,
+    type: "reply",
+  });
+
+  try {
+    comment = await Comment.findById(commentId);
+  } catch (error) {
+    return next(error);
+  }
+
+  if (!comment) {
+    const error = new HttpError("the comment you replied does'nt exist.", 401);
+    return next(error);
+  }
+
+  try {
+    post = await Post.findById(postId);
+  } catch (error) {
+    return next(error);
+  }
+
+  if (!post) {
+    const error = new HttpError("Post doesn't exist.", 500);
+    return next(error);
+  }
+
+  const notification = new Notification({
+    owner: post.creator,
+    user: {
+      id: postedComment.writer.id,
+      username: postedComment.writer.username,
+    },
+    message: "replied your comment:" + " " + comment.message,
+    date: new Date(),
+  });
+
+  try {
+    await notification.save();
+    await postedComment.save();
+    comment.replies.push(postedComment._id);
+    post.comments_length++;
+    await post.save();
+  } catch (error) {
+    return next(error);
+  }
+
+  res.status(201).json({ message: "Comment posted!" });
+};
+
 const getCommentsByPostId = async (req, res, next) => {
   const postId = req.params.pid;
 
@@ -74,6 +146,34 @@ const getCommentsByPostId = async (req, res, next) => {
   }
 
   res.status(200).json({ comments: comments });
+};
+
+const getRepliesByCommentId = async (req, res, next) => {
+  const commentId = req.params.cid;
+
+  let replies;
+  let comment;
+
+  try {
+    comment = await Comment.findById(commentId);
+  } catch (error) {
+    return next(error);
+  }
+
+  if (!comment) {
+    const error = new HttpError("Comment doesn't exist.");
+    return next(error);
+  }
+
+  try {
+    replies = await Comment.find({ replies: { $in: comment.replies } }).sort({
+      date: -1,
+    });
+  } catch (error) {
+    return next(error);
+  }
+
+  res.status(200).json({ replies: replies });
 };
 
 const deleteComment = async (req, res, next) => {
@@ -113,3 +213,5 @@ const deleteComment = async (req, res, next) => {
 exports.getCommentsByPostId = getCommentsByPostId;
 exports.postComment = postComment;
 exports.deleteComment = deleteComment;
+exports.getRepliesByCommentId = getRepliesByCommentId;
+exports.postReply = postReply;
