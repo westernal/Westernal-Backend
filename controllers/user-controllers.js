a70f8be838cf16df8842350dd833c6676fc0545a
ali navidi <ali_navidi24@yahoo.com>
2022-08-21 09:29:55 +0430

    support email changed

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -356,10 +356,7 @@ const resetPassword = async (req, res, next) => {
 
   try {
     var mailOptions = {
-      from: {
-        name: "Westernal",
-        address: "support@westernal.net",
-      },
+      from: "Westernal <support@westernal.net>",
       to: email,
       subject: "Reset your Westernal password",
       html: ` <div class="email" style="width: 100%; height:100%; text-align: center;">
