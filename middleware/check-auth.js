7cdc5d29ba830ebc0a4cb4ee620bcc6feaee22f8
ali navidi <ali_navidi24@yahoo.com>
2023-03-03 21:49:19 +0330

    security optimized

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,24 +1,22 @@
-const HttpError = require('../models/http-error');
-
-const jwt = require('jsonwebtoken');
+const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
+import { SECRET_KEY } from "../security";
 
 module.exports = (req, res, next) => {
-
-    if (req.method === 'OPTIONS') {
-        return next()
-    }
-    try {
-        const token = req.headers.authorization.split(' ')[1]
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
     if (!token) {
-       throw new HttpError('Token is not valid!', 401)
+      throw new HttpError("Token is not valid!", 401);
     }
 
-    const decodedToken = jwt.verify(token, 'secret_key')
-    req.userData = { userId: decodedToken.userId }
-    next()
-
-    } catch (error) {
-       return next(error) 
-    }
-}
+    const decodedToken = jwt.verify(token, SECRET_KEY);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
--- middleware/check-cookie.js
+++ middleware/check-cookie.js
@@ -1,16 +1,17 @@
 const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
+import { SECRET_KEY } from "../security";
 
 module.exports = (req, res, next) => {
   const { cookies } = req;
   const refreshToken = cookies.refreshToken;
 
-  try {
-    if (!refreshToken) {
-      throw new HttpError("Authentication failed!", 403);
-    }
+  if (!refreshToken) {
+    throw new HttpError("Authentication failed!", 403);
+  }
 
-    const decodedToken = jwt.verify(jwt, "secret_key");
+  try {
+    const decodedToken = jwt.verify(jwt, SECRET_KEY);
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (error) {
